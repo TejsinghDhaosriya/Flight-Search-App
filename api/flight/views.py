004843c66389eab3c5d6b0010d07ba7478f32920
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-11-12 21:58:35 +0530

    Done with Shortest Route

--- api/flight/views.py
+++ api/flight/views.py
@@ -78,101 +78,228 @@ def flightSearch(request):
     #     return Response(serializer.data)
     # else:
     #     return Response("Not Available Flight Plan")
-    print('Body datta............')
-    print(request.data)
-    all_flight_info = Flight.objects.all()
+    # print('Body datta............')
+    # print(request.data)
+    
+    dt=request.data.get("departure_time") 
+    # print(dt)
+    # all_flight_info = Flight.objects.all()
+    # print('-------eln--------',len(all_flight_info))
+    all_flight_info = Flight.objects.filter(departure_time__gte=dt)
+    # print('-------eln--------',len(all_flight_info2))
     # print(all_flight_info)
     # print(len(all_flight_info))
-    print(all_flight_info[0].number)
+    # print(all_flight_info[0].number)
 
-    from collections import defaultdict
+    import collections
+    import heapq
 
-    class Graph():
-        def __init__(self):
-            """
-            self.edges is a dict of all possible next nodes
-            e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-            self.weights has all the weights between two nodes,
-            with the two nodes as a tuple as the key
-            e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-            """
-            self.edges = defaultdict(list)
-            self.weights = {}
-        
-        def add_edge(self, from_node, to_node, weight):
-            # Note: assumes edges are bi-directional
-            self.edges[from_node].append(to_node)
-            self.edges[to_node].append(from_node)
-            self.weights[(from_node, to_node)] = weight
-            self.weights[(to_node, from_node)] = weight
-    graph = Graph()
+    def shortestPath(edges, source, sink):
+        # create a weighted DAG - {node:[(cost,neighbour), ...]}
+        graph = collections.defaultdict(list)
+        link=0;
+        for l, r, c ,inp,fin in edges:
+            graph[l].append((c,r,inp,fin))
+        print(graph)
+        # create a priority queue and hash set to store visited nodes
     
+        queue, visited = [(0, source,[],[],[])], [[],[]]
+        heapq.heapify(queue)
+        # traverse graph with BFS
+        while queue:
+            (cost, node, path,inp, fin) = heapq.heappop(queue)
+            print('cost :',cost,'node :',node,'path :',path,'in: ',inp,'fin: ',fin)
+            # visited.append((source,graph[2]))
+            # visit the node if it was not visited before
+            print('checkign..........................')
+            
+            # st =len(visited)
+            # if st==0:
+            #     s=0
+            # else:
+            #     s =visited[(len(visited)-1)][1][1]
+                
+
+            if node not in visited:# and s<=inp:
+                visited.append((node,[inp,fin]))
+                path = path + [node]
+                # hit the sink
+                if node == sink:
+                    return (cost, path)
+                # visit neighbours
+                i=0
+                for c, neighbour,inp,fin in graph[node]:
+                    print('yaha pe hu mein',visited[(len(visited)-1)][1][1],neighbour,node ,graph[node],visited[(len(visited)-1)])
+                    
+                    s =visited[(len(visited)-1)][1][1]
+                    if type(s)==type([]):
+                        s=0
+                    print(type(s))
+                    print(s)                                      
+                    print(s,'---------------------',inp)
+
+                    if neighbour not in visited and s<=inp:
+                        print('neighbour is :',neighbour)
+                        heapq.heappush(queue, (cost+c, neighbour, path,inp,fin))
+                        print(queue)
+        return str("No Flight Available")
+
+
     edges = []
     for element in all_flight_info:
-        edges.append((element.departure_city,element.arrival_city,(element.arrival_time - element.departure_time)))
-    print(edges)
-    #     ('X', 'A', 7),
-    #     ('X', 'B', 2),
-    #     ('X', 'C', 3),
-    #     ('X', 'E', 4),
-    #     ('A', 'B', 3),
-    #     ('A', 'D', 4),
-    #     ('B', 'D', 4),
-    #     ('B', 'H', 5),
-    #     ('C', 'L', 2),
-    #     ('D', 'F', 1),
-    #     ('F', 'H', 3),
-    #     ('G', 'H', 2),
-    #     ('G', 'Y', 2),
-    #     ('I', 'J', 6),
-    #     ('I', 'K', 4),
-    #     ('I', 'L', 4),
-    #     ('J', 'L', 1),
-    #     ('K', 'Y', 5),
-    # ]
+        edges.append((element.departure_city,element.arrival_city,(element.arrival_time - element.departure_time),element.departure_time,element.arrival_time))
+    # print(edges)
 
-    for edge in edges:
-        graph.add_edge(*edge)
+    dc=request.data.get("departure_city")    
+    ac=request.data.get("arrival_city") 
+    flight_data = shortestPath(edges,dc,ac)   
+    # print(flight_data)
+    route_cost =flight_data[0]
+    route_data=flight_data[1]
+    route_data_len = len(route_data)
+    total_flights=[]
+    for element in all_flight_info:
+        total_flights.append((element.departure_city,element.arrival_city,(element.arrival_time - element.departure_time),element.departure_time,element.arrival_time,element.number))
+        # print(total_flights)
+
+
+    #sorting the array by 3 position element cost
+
+    def sortSecond(val): 
+        return val[2]  
+    
+    # list1 to demonstrate the use of sorting  
+    # using using second key  
+    list1 = total_flights
+    # sorts the array in ascending according to  
+    # second element 
+    list1.sort(key = sortSecond)  
+    # print('----------------')
+    # print(list1) 
+    final_output=[]
+    arrival_cost =0
+#     import time
+
+# s = time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(epoch_time))
+
+    for i in range(1,route_data_len):
         
+        for j in range(1,len(list1)+1):
+            if route_data[i-1]==list1[j-1][0] and route_data[i]==list1[j-1][1]:
+                final_output.append({'departure_city':route_data[i-1],
+                                      'departure_time':list1[j-1][3],
+                                      'arrival_city':route_data[i],
+                                      'arrival_time':list1[j-1][4],
+                                      'number':list1[j-1][5]
+                                     })
+                break
 
-    def dijsktra(graph, initial, end):
-        # shortest paths is a dict of nodes
-        # whose value is a tuple of (previous node, weight)
-        shortest_paths = {initial: (None, 0)}
-        current_node = initial
-        visited = set()
+                 
+
+        # print(i)
+
+    print(final_output)
+    data = {
+        'route_cost':route_cost ,
+        'data': final_output
         
-        while current_node != end:
-            visited.add(current_node)
-            destinations = graph.edges[current_node]
-            weight_to_current_node = shortest_paths[current_node][1]
+    }
+    return Response(data)
 
-            for next_node in destinations:
-                weight = graph.weights[(current_node, next_node)] + weight_to_current_node
-                if next_node not in shortest_paths:
-                    shortest_paths[next_node] = (current_node, weight)
-                else:
-                    current_shortest_weight = shortest_paths[next_node][1]
-                    if current_shortest_weight > weight:
-                        shortest_paths[next_node] = (current_node, weight)
+
+
+
+#     def flightSearch():
+#         from collections import defaultdict 
+
+#         #This class represents a directed graph using adjacency list representation 
+#         class Graph: 
+
+#             def __init__(self,vertices): 
+#                 self.V = vertices #No. of vertices 
+#                 self.V_org = vertices 
+#                 self.graph = defaultdict(list) # default dictionary to store graph 
+
+#             # function to add an edge to graph 
+#             def addEdge(self,u,v,w): 
+#                 if w == 1: 
+#                     self.graph[u].append(v) 
+#                 else:	 
+#                     '''split all edges of weight 2 into two 
+#                     edges of weight 1 each. The intermediate 
+#                     vertex number is maximum vertex number + 1, 
+#                     that is V.'''
+#                     self.graph[u].append(self.V) 
+#                     self.graph[self.V].append(v) 
+#                     self.V = self.V + 1
             
-            next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
-            if not next_destinations:
-                return "Route Not Possible"
-            # next node is the destination with the lowest weight
-            current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
-        
-        # Work back through destinations in shortest path
-        path = []
-        while current_node is not None:
-            path.append(current_node)
-            next_node = shortest_paths[current_node][0]
-            current_node = next_node
-        # Reverse path
-        path = path[::-1]
-        return path
-        
-        
-        
-    print(dijsktra(graph,request.data.get("departure_city"),request.data.get("departure_city")))
-    return Response("Hi")
+#             # To print the shortest path stored in parent[] 
+#             def printPath(self, parent, j): 
+#                 Path_len = 1
+#                 if parent[j] == -1 and j < self.V_org : #Base Case : If j is source 
+#                     print j, 
+#                     return 0 # when parent[-1] then path length = 0	 
+#                 l = self.printPath(parent , parent[j]) 
+
+#                 #incerement path length 
+#                 Path_len = l + Path_len 
+
+#                 # print node only if its less than original node length. 
+#                 # i.e do not print a ny new node that has been added later 
+#                 if j < self.V_org : 
+#                     print j, 
+
+#                 return Path_len 
+
+#             ''' This function mainly does BFS and prints the 
+#                 shortest path from src to dest. It is assumed 
+#                 that weight of every edge is 1'''
+#             def findShortestPath(self,src, dest): 
+
+#                 # Mark all the vertices as not visited 
+#                 # Initialize parent[] and visited[] 
+#                 visited =[False]*(self.V) 
+#                 parent =[-1]*(self.V) 
+
+#                 # Create a queue for BFS 
+#                 queue=[] 
+
+#                 # Mark the source node as visited and enqueue it 
+#                 queue.append(src) 
+#                 visited[src] = True
+
+#                 while queue : 
+                    
+#                     # Dequeue a vertex from queue 
+#                     s = queue.pop(0) 
+                    
+#                     # if s = dest then print the path and return 
+#                     if s == dest: 
+#                         return self.printPath(parent, s) 
+                        
+
+#                     # Get all adjacent vertices of the dequeued vertex s 
+#                     # If a adjacent has not been visited, then mark it 
+#                     # visited and enqueue it 
+#                     for i in self.graph[s]: 
+#                         if visited[i] == False: 
+#                             queue.append(i) 
+#                             visited[i] = True
+#                             parent[i] = s 
+
+
+#         # Create a graph given in the above diagram 
+#         g = Graph(4) 
+#         g.addEdge(0, 1, 2) 
+#         g.addEdge(0, 2, 2) 
+#         g.addEdge(1, 2, 1) 
+#         g.addEdge(1, 3, 1) 
+#         g.addEdge(2, 0, 1) 
+#         g.addEdge(2, 3, 2) 
+#         g.addEdge(3, 3, 2) 
+
+#         src = 0; dest = 3
+#         print ("Shortest Path between %d and %d is " %(src, dest)), 
+#         l = g.findShortestPath(src, dest) 
+#         print ("\nShortest Distance between %d and %d is %d " %(src, dest, l)), 
+# #https://www.geeksforgeeks.org/shortest-path-weighted-graph-weight-edge-1-2/
