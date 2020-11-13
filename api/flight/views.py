2f952f4cc37f50803ef21e2aa77da36c2e3e166e
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-11-13 20:23:35 +0530

    patch

--- api/flight/views.py
+++ api/flight/views.py
@@ -202,28 +202,42 @@ def flightSearch(request):
 ############################################# Shortest Path By ID #############################
 @api_view(['POST'])
 def flightSearch2(request):
+    # tasks=Flight.objects.filter(departure_city=request.data.get("departure_city") , arrival_city=request.data.get("arrival_city"))
+    # if len(tasks)>0:
+    #     serializer = FlightSerializer(tasks,many=True)
+    #     return Response(serializer.data)
+    # else:
+    #     return Response("Not Available Flight Plan")
+    # print('Body datta............')
+    # print(request.data)
     
     dt=request.data.get("departure_time") 
+    # print(dt)
+    # all_flight_info = Flight.objects.all()
+    # print('-------eln--------',len(all_flight_info))
     all_flight_info = Flight.objects.filter(departure_time__gte=dt)
+    # print('-------eln--------',len(all_flight_info2))
+    # print(all_flight_info)
+    # print(len(all_flight_info))
+    # print(all_flight_info[0].number)
 
     import collections
     import heapq
-    check=[]
+
     def shortestPath(edges, source, sink):
         # create a weighted DAG - {node:[(cost,neighbour), ...]}
         graph = collections.defaultdict(list)
         link=0;
-        for l, r, c ,inp,fin,unique_id in edges:
-            graph[l].append((c,r,inp,fin,unique_id))
+        for l, r, c ,inp,fin in edges:
+            graph[l].append((c,r,inp,fin))
         #print(graph)
         # create a priority queue and hash set to store visited nodes
     
-        queue, visited = [(0, source,[],[],[],[])], [[],[]]
+        queue, visited = [(0, source,[],[],[])], [[],[]]
         heapq.heapify(queue)
-        
         # traverse graph with BFS
         while queue:
-            (cost, node, path,inp, fin,unique_id) = heapq.heappop(queue)
+            (cost, node, path,inp, fin) = heapq.heappop(queue)
             #print('cost :',cost,'node :',node,'path :',path,'in: ',inp,'fin: ',fin)
             # visited.append((source,graph[2]))
             # visit the node if it was not visited before
@@ -238,33 +252,31 @@ def flightSearch2(request):
             if node not in visited:# and s<=inp:
                 visited.append((node,[inp,fin]))
                 path = path + [node]
-                check.append(unique_id) 
-                print('Path is :  ',path)
-                print('Path is :  ',check)
                 # hit the sink
                 if node == sink:
                     return (cost, path)
                 # visit neighbours
                 i=0
-                for c, neighbour,inp,fin,unique_id in graph[node]:
+                for c, neighbour,inp,fin in graph[node]:
                     s =visited[(len(visited)-1)][1][1]
                     if type(s)==type([]):
                         s=0
                    
                     if neighbour not in visited and s<=inp:
-                        heapq.heappush(queue, (cost+c, neighbour, path,inp,fin,unique_id))
+                        heapq.heappush(queue, (cost+c, neighbour, path,inp,fin))
                         
         return str("No Flight Available")
 
+
     edges = []
     for element in all_flight_info:
-        edges.append((element.departure_city,element.arrival_city,(element.arrival_time - element.departure_time),element.departure_time,element.arrival_time,element.id))
+        edges.append((element.departure_city,element.arrival_city,(element.arrival_time - element.departure_time),element.departure_time,element.arrival_time))
     # print(edges)
 
     dc=request.data.get("departure_city")    
     ac=request.data.get("arrival_city") 
     flight_data = shortestPath(edges,dc,ac)   
-    # print(flight_data)
+    print(flight_data)
     route_cost =flight_data[0]
     route_data=flight_data[1]
     route_data_len = len(route_data)
@@ -272,7 +284,7 @@ def flightSearch2(request):
     for element in all_flight_info:
         total_flights.append((element.departure_city,element.arrival_city,(element.arrival_time - element.departure_time),element.departure_time,element.arrival_time,element.number))
         # print(total_flights)
-
+    
 
     #sorting the array by 3 position element cost
 
@@ -289,25 +301,69 @@ def flightSearch2(request):
     # print(list1) 
     final_output=[]
     arrival_cost =0
+#     import time
 
-    for ids in range(1,len(check)):
-        fR = Flight.objects.get(id=check[ids])
-        final_output.append({'departure_city':fR.departure_city,
-                                      'departure_time':fR.departure_time,
-                                      'arrival_city':fR.arrival_city,
-                                      'arrival_time':fR.arrival_time,
-                                      'number':fR.number
-                                     })        
 
-    print(final_output)
+# s = time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(epoch_time))
+    # test =0
+    # test_array=[]
+    # for i in range(1,route_data_len):
+        
+    #     for j in range(1,len(list1)+1):
+    #         if (route_data[i-1]==list1[j-1][0] and route_data[i]==list1[j-1][1] and test <=list1[j-1][3]):
+    #             test_array.append([list1[j-1][0],list1[j-1][1],0,list1[j-1][3],list1[j-1][4],list1[j-1][5]])
+    
+    # print(test_array)
+    # test =0
+    # sum=0
+    # while True:
+    #     for i in range(1,route_data_len):
+            
+    #         for j in range(1,len(list1)+1):
+    #             if (route_data[i-1]==test_array[j-1][0] and route_data[i]==test_array[j-1][1] and test_array[j-1][2]==0):
+                
+    #                 final_output.append({'departure_city':route_data[i-1],
+    #                                     'departure_time':test_array[j-1][3],
+    #                                     'arrival_city':route_data[i],
+    #                                     'arrival_time':test_array[j-1][4],
+    #                                     'number':test_array[j-1][5]
+    #                                     })
+    #                 test_array[j-1][2]=1
+    #                 print(test_array[j-1][2])  
+    #                 print(test_array)
+    #                 sum+=(list1[j-1][4]-list1[j-1][3])                           
+    #                 print(sum,'---------',route_cost) 
+    #                 break
+
+    #     if sum==route_cost: 
+    #         print(sum,'---------',route_cost) 
+    #         break
+    #     else:
+    #         final_output.clear()      
+            
+    
+    test =0
+    for i in range(1,route_data_len):
+        
+        for j in range(1,len(list1)+1):
+            if (route_data[i-1]==list1[j-1][0] and route_data[i]==list1[j-1][1] and test <=list1[j-1][3]):
+                test =list1[j-1][3]
+                final_output.append({'departure_city':route_data[i-1],
+                                      'departure_time':list1[j-1][3],
+                                      'arrival_city':route_data[i],
+                                      'arrival_time':list1[j-1][4],
+                                      'number':list1[j-1][5]
+                                     })
+                break
+
+                 
+
+        # print(i)
+
+    #print(final_output)
     data = {
         'route_cost':route_cost ,
         'data': final_output
         
     }
     return Response(data)
-
-
-
-
-
